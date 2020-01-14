ab1e06bcf2a0d219fc866928ec8bde223b0117c2
sontayvo <taysonvo@gmail.com>
2020-01-14 08:07:34 +0700

    Updates

---
+++ precache-manifest.50e2ad4d920ac9fc5495d94889df85f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "893120192956d94f3dd1a8ce620a1a46",
+    "url": "/simple-weather-reactjs/index.html"
+  },
+  {
+    "revision": "312fe57c023dfa91cb7e",
+    "url": "/simple-weather-reactjs/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6348541a8e90739ed673",
+    "url": "/simple-weather-reactjs/static/css/main.3c671d66.chunk.css"
+  },
+  {
+    "revision": "312fe57c023dfa91cb7e",
+    "url": "/simple-weather-reactjs/static/js/2.e2f9f0d7.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/simple-weather-reactjs/static/js/2.e2f9f0d7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6348541a8e90739ed673",
+    "url": "/simple-weather-reactjs/static/js/main.8d9afac6.chunk.js"
+  },
+  {
+    "revision": "4983b4adf769527277e4",
+    "url": "/simple-weather-reactjs/static/js/runtime-main.adfd8051.js"
+  }
+]);
